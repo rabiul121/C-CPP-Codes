b6faab1c2c2bfe64804bde200e84e2300b843397
Md. Rabiul Islam <91551048+rabiul121@users.noreply.github.com>
2024-02-01 22:35:18 +0600

    added <math.h> codes

---
+++ 10_math.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double A = sqrt(81);
+    double B = pow(2, 3);
+    int C = round(3.1416);
+    int D = ceil(2.14);
+    int E = floor(89.56);
+    double F = fabs(-156);
+    double G = log(10);
+    double H = sin(45);
+    double I = cos(45);
+    double J = tan(90);
+
+    printf("\nResult = %f\n\n", J);
+    return 0;
+}
