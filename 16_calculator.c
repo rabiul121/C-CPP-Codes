9736498ecedad8794d2cce8ce5ea11be6dc4e8e1
Md. Rabiul Islam <91551048+rabiul121@users.noreply.github.com>
2024-02-04 22:28:28 +0600

    calculator code added

---
+++ 16_calculator.c
@@ -0,0 +1,45 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+  char operator;
+  double num1;
+  double num2;
+  double result;
+
+  printf("\nEnter an operator (+ - * /): ");
+  scanf("%c", &operator);
+
+  printf("\nEnter number 1: ");
+  scanf("%lf", &num1);
+
+  printf("\nEnter number 2: ");
+  scanf("%lf", &num2);
+
+  switch (operator) {
+  case '+':
+    result = num1 + num2;
+    printf("\nResult: %.2lf", result);
+    break;
+
+  case '-':
+    result = num1 - num2;
+    printf("\nResult: %.2lf", result);
+    break;
+
+  case '*':
+    result = num1 * num2;
+    printf("\nResult: %.2lf", result);
+    break;
+
+  case '/':
+    result = num1 / num2;
+    printf("\nResult: %.2lf", result);
+    break;
+
+  default:
+    printf("\nEnter valid operator!");
+  }
+
+  return 0;
+}
