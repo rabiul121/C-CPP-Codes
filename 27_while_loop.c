ddffff99d230de3d780035c4c651be5e96b2ba46
Md. Rabiul Islam <91551048+rabiul121@users.noreply.github.com>
2024-02-05 20:40:04 +0600

    added while loop code

---
+++ 27_while_loop.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char name[20];
+  printf("\nWhat's your name? ");
+  fgets(name, 20, stdin);
+  name[strlen(name) - 1] = '\0';
+
+  while (strlen(name) == 0) {
+    printf("\nYou did not enter your name! ");
+    printf("\nWhat's your name? ");
+    fgets(name, 20, stdin);
+    name[strlen(name) - 1] = '\0';
+  }
+  printf("Hello, %s!\n", name);
+  return 0;
+}
